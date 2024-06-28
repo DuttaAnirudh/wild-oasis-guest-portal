8c0f7d9b4ec611e1dd1d1fd05fd0441e08809884
DuttaAnirudh <worktodutta@gmail.com>
2024-06-28 22:15:06 +0530

    feat: build layout; add interactivity with client components; add a loading indicator

--- app/about/page.js
+++ app/about/page.js
@@ -1,9 +1,6 @@
-import Navigation from "../components/Navigation";
-
 export default function Page() {
   return (
     <div>
-      <Navigation />
       <h2>About page</h2>
     </div>
   );
--- app/account/page.js
+++ app/account/page.js
@@ -1,9 +1,6 @@
-import Navigation from "../components/Navigation";
-
 export default function Page() {
   return (
     <div>
-      <Navigation />
       <h2>Account page</h2>
     </div>
   );
