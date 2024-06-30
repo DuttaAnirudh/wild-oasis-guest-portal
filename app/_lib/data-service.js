063074c027bf745f940858e89334df3750879127
DuttaAnirudh <worktodutta@gmail.com>
2024-06-30 23:36:36 +0530

    add nested routes and related pages to accounts page; add content and styles to account page and its routes

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -1,5 +1,16 @@
 /////////////
 // GET
+export async function getCountries() {
+  try {
+    const res = await fetch(
+      "https://restcountries.com/v2/all?fields=name,flag"
+    );
+    const countries = await res.json();
+    return countries;
+  } catch {
+    throw new Error("Could not fetch countries");
+  }
+}
 
 /////////////
 // CREATE
