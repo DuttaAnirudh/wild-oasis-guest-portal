b815c469154ea552e09b46e1081bec1ba2c02b8d
DuttaAnirudh <worktodutta@gmail.com>
2024-07-01 19:42:50 +0530

    display cabins info on cabins page; create dynamic route segments for all all the cabins and display respective data; add loading spinner

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -1,3 +1,5 @@
+import { supabase } from "./supabase";
+
 /////////////
 // GET
 export async function getCountries() {
@@ -10,6 +12,36 @@ export async function getCountries() {
   } catch {
     throw new Error("Could not fetch countries");
   }
+}
+
+// Get the list of cabins with their data
+export const getCabins = async function () {
+  const { data, error } = await supabase
+    .from("cabins")
+    .select("id, name, maxCapacity, regularPrice, discount, image")
+    .order("name");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be loaded");
+  }
+
+  return data;
+};
+
+// Get data about a particular cabin with id
+export async function getCabin(id) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .select("*")
+    .eq("id", id)
+    .single();
+
+  if (error) {
+    console.error(error);
+  }
+
+  return data;
 }
 
 /////////////
