b784731a2d2e839f94141747a78106288fb244ac
DuttaAnirudh <worktodutta@gmail.com>
2024-07-14 21:42:37 +0530

    improve date selector functionality and replace test data with real time data

--- app/_components/DateSelector.js
+++ app/_components/DateSelector.js
@@ -1,6 +1,11 @@
 "use client";
 
-import { isWithinInterval } from "date-fns";
+import {
+  differenceInDays,
+  isPast,
+  isSameDay,
+  isWithinInterval,
+} from "date-fns";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
 import { useReservation } from "@/app/_context/ReservationContext";
@@ -22,17 +27,20 @@ function DateSelector({ cabin, settings, bookedDates }) {
 
   const { range, setRange, resetRange } = useReservation();
 
-  // TEST
-  const numNights = 23;
-  const cabinPrice = 23;
+  // Checking if the selected date is b/w an already booked date
+  // If TRUE, unselect the selected date
+  const displayRange = isAlreadyBooked(range, bookedDates) ? {} : range;
+
+  const numNights = differenceInDays(displayRange.to, displayRange.from);
+  const cabinPrice = numNights * (regularPrice - discount);
 
   return (
     <div className="flex flex-col justify-between">
       <DayPicker
         className="pt-12 place-self-center"
         mode="range"
-        onSelect={(range) => setRange(range)}
-        selected={range}
+        onSelect={setRange}
+        selected={displayRange}
         min={minBookingLength + 1}
         max={maxBookingLength}
         fromMonth={new Date()}
@@ -40,6 +48,10 @@ function DateSelector({ cabin, settings, bookedDates }) {
         toYear={new Date().getFullYear() + 5}
         captionLayout="dropdown"
         numberOfMonths={2}
+        disabled={(curDate) =>
+          isPast(curDate) ||
+          bookedDates.some((date) => isSameDay(date, curDate))
+        }
       />
 
       <div className="flex items-center justify-between px-8 bg-accent-500 text-primary-800 h-[72px]">
