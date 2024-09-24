ac644af69908082850dfca9008a2d4abcaab1eb1
DuttaAnirudh <worktodutta@gmail.com>
2024-09-24 19:21:23 +0530

    add responsiveness to payment confirmation page and password recovery pages

--- app/recovery/forgot-password/confirm/page.js
+++ app/recovery/forgot-password/confirm/page.js
@@ -1,6 +1,6 @@
 export default function Page() {
   return (
-    <h4 className="text-3xl text-accent-400 mt-20 flex items-center justify-center">
+    <h4 className="max-[400px]:text-xl text-2xl sm:text-3xl text-accent-400 mt-20 flex items-center justify-center text-center px-2">
       Password Reset Link has sent to your provided Email Address.
     </h4>
   );
--- app/recovery/reset-password/page.js
+++ app/recovery/reset-password/page.js
@@ -9,8 +9,8 @@ export default function Page({ searchParams }) {
   const sessionCode = searchParams.code;
 
   return (
-    <div className="flex flex-col items-center justify-center h-full w-[30rem] mx-auto">
-      <div className="flex flex-col items-center justify-center gap-2 w-[28rem] border border-accent-400 py-16 px-10 rounded-2xl">
+    <div className="flex flex-col items-center justify-center h-full max-w-[30rem] sm:w-[30rem] mx-auto px-2 sm:px-4 xl:px-0">
+      <div className="flex flex-col items-center justify-center gap-2 max-w-[28rem] sm:w-[28rem] sm:border border-accent-400 sm:py-16 sm:px-10 rounded-2xl">
         <Logo />
 
         <h3 className="my-4 text-accent-500 text-xl font-light capitalize">
